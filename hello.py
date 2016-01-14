0e35d4ea656916adc8aff9fc5e945cbab6095862
Tchomp <tswain2@mail.valenciacollege.edu>
2016-01-13 22:05:28 -0500

    Step 6 of Instructions

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is Timothy Swain'
+	return 'hello world, my name is '
 
-print( hello_world() + 'hector' )
+print( hello_world() + 'Timothy Swain' )
